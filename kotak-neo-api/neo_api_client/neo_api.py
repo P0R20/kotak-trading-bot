14ba714d2b2c6e387ef946fd05e1148c2ed5c898
P0R20 <nirmaljain1983@gmail.com>
2025-07-12 01:48:32 +0530

    🚀 Added set_callback and subscribe to NeoAPI

--- kotak-neo-api/neo_api_client/neo_api.py
+++ kotak-neo-api/neo_api_client/neo_api.py
@@ -2,3 +2,18 @@ class NeoAPI:
     def __init__(self, consumer_key, consumer_secret, environment="prod"):
         # your logic here
         pass
+# --- PATCHED METHODS ---
+
+def set_callback(self, event_type, callback):
+    if not hasattr(self, "_callbacks"):
+        self._callbacks = {}
+    self._callbacks[event_type] = callback
+
+def subscribe(self, instruments, action="subscribe"):
+    if not hasattr(self, "_ws") or self._ws is None:
+        raise Exception("WebSocket not connected")
+    payload = {
+        "action": action,
+        "params": instruments
+    }
+    self._ws.send(json.dumps(payload))
